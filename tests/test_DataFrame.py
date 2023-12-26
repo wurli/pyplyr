a4a44c0432e9e216f43eb17f6db0d3d53d8d0869
Jacob Scott <jake12358@gmail.com>
2023-12-26 16:23:21 +0000

    add test for df printing

--- tests/test_DataFrame.py
+++ tests/test_DataFrame.py
@@ -21,4 +21,16 @@ def test_df_creation():
     npt.assert_array_equal(df['z'], np.array([3, 4, 5]))
 
 
+def test_df_printing(capsys, snapshot):
+    snapshot.snapshot_dir = 'tests/snapshots'
     
+    df = pyp.DataFrame(
+        x=['foo', 'foo', 'bar'],
+        y=[11.22, 2.3, 3.4],
+        z=[3, 4, 5]
+    ) 
+    
+    print(df)
+    
+    captured = capsys.readouterr()
+    snapshot.assert_match(captured.out, 'test_df_printing.txt')
